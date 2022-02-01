97fa8996f4d6b4668168fb3066f2d1f3da580b02
Lindsay Wilhelm <lindsaywilhelm11@gmail.com>
2022-02-01 01:09:27 +0000

    completes exercise 5 and uses the sum and average methods and counts how many stores are over 1mil

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,17 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+# Output the total revenue for the entire company (all stores), using 
+#     Active Record's .sum calculation method.
+# On the next line, also output the average annual revenue for all stores.
+# Output the number of stores that are generating $1M or more in annual sales. 
+#     Hint: Chain together where and size (or count) Active Record methods.
+
+@store_sum = Store.sum(:annual_revenue)
+@store_average = Store.average(:annual_revenue)
+@store_millions = Store.where("annual_revenue > 1000000").count
+
+puts @store_sum
+puts @store_average
+puts @store_millions
